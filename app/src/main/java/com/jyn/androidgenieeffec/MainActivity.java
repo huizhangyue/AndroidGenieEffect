75fa25c8c95ba007f877d8c28027f6696014b4bb
jiaoyaning <jiaoyaning@koolearn-inc.com>
2019-07-25 12:31:31 +0800

    view 转 bitmap

--- app/src/main/java/com/jyn/androidgenieeffec/MainActivity.java
+++ app/src/main/java/com/jyn/androidgenieeffec/MainActivity.java
@@ -2,13 +2,58 @@ package com.jyn.androidgenieeffec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.jyn.genieeffec.GenieEffectLayout;
+
+/**
+ * @author jiao
+ */
+@SuppressLint("NewApi")
 public class MainActivity extends AppCompatActivity {
+
+    private GenieEffectLayout genieEffectLayout;
+    Button button;
+    ImageView imageView;
+    ImageView imageView2;
+    LinearLayout layout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        genieEffectLayout = findViewById(R.id.genie_effect_layout);
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.image);
+        imageView2 = findViewById(R.id.image2);
+        layout = findViewById(R.id.layout);
+
+        genieEffectLayout
+                .setMaximizeView(layout)
+                .setMinimizeView(button)
+                .init();
+    }
+
+    public void minimize(View view) {
+        genieEffectLayout.start();
+//        Bitmap bm = genieEffectLayout.createBitmap(layout);
+//        if (bm == null) {
+//            Toast.makeText(this, "bm为空", Toast.LENGTH_SHORT).show();
+//        }
+//        Log.i("main", "bm:" + bm.getAllocationByteCount() / 1024);
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_bg);
+//        Log.i("main", "bitmap:" + bitmap.getAllocationByteCount() / 1024);
+//        imageView2.setImageBitmap(bm);
     }
 }
--- genieeffect/src/main/java/com/jyn/genieeffec/GenieEffectLayout.java
+++ genieeffect/src/main/java/com/jyn/genieeffec/GenieEffectLayout.java
@@ -1,24 +1,144 @@
 package com.jyn.genieeffec;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import androidx.annotation.RequiresApi;
 
 /**
  * Created by jiao on 2019/7/24.
  */
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
 public class GenieEffectLayout extends RelativeLayout {
+
+    Context context;
+
+    /**
+     * 最大化的view
+     */
+    View maximizeView;
+
+    /**
+     * 最小化的view
+     */
+    View minimizeView;
+
+    /**
+     * 把目标view转后后得到的bitmap
+     */
+    Bitmap mBitmap;
+
+    Paint mPaint;
+
+    MeshHelper mMeshHelper;
+
+    /**
+     * 最大化View的位置坐标
+     */
+    int maximizeLeft;
+    int maximizeTop;
+    int maximizeRight;
+    int maximizeBottom;
+
+    /**
+     * 最大化view的宽高值
+     */
+    int maximizeWidth;
+    int maximizeHeight;
+
+    boolean isStart;
 
     public GenieEffectLayout(Context context) {
         super(context);
+        initLayout(context);
     }
 
     public GenieEffectLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initLayout(context);
     }
 
     public GenieEffectLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initLayout(context);
     }
-    
+
+    private void initLayout(Context context) {
+        this.context = context;
+        setBackground(null);
+        setWillNotDraw(false);
+    }
+
+    public GenieEffectLayout setMaximizeView(final View view) {
+        maximizeView = view;
+        view.post(() -> {
+            maximizeLeft = view.getLeft();
+            maximizeTop = view.getTop();
+        });
+        return this;
+    }
+
+    public GenieEffectLayout setMinimizeView(View view) {
+        minimizeView = view;
+        return this;
+    }
+
+    public void init() {
+        this.post(() -> {
+            mBitmap = loadBitmapFromView(maximizeView);
+            maximizeWidth = mBitmap.getWidth();
+            maximizeHeight = mBitmap.getHeight();
+            Log.i("main", "maximizeWidth:" + maximizeWidth);
+            Log.i("main", "maximizeHeight:" + maximizeHeight);
+            mPaint = new Paint();
+            mPaint.setAntiAlias(true);
+            mMeshHelper = new MeshHelper();
+        });
+    }
+
+    public void start() {
+        isStart = true;
+        invalidate();
+        maximizeView.setVisibility(GONE);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (isStart) {
+            canvas.drawBitmap(mBitmap, maximizeLeft, maximizeTop, mPaint);
+        }
+    }
+
+    /**
+     * 把view转换成bitmap
+     *
+     * @param v 目标view
+     * @return 转换后得到的bitmap
+     */
+    public Bitmap loadBitmapFromView(View v) {
+        int height = v.getHeight();
+        int width = v.getWidth();
+        Log.i("main", "v height:" + v.getHeight());
+        Log.i("main", "v width:" + v.getWidth());
+
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmp);
+        c.drawColor(Color.WHITE);
+        // 如果不设置canvas画布为白色，则生成透明
+        v.layout(v.getLeft(), v.getTop(), height, width);
+        v.draw(c);
+        return bmp;
+    }
 }
